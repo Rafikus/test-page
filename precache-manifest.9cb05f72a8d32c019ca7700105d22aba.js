2a5cf3fa0e062c7511fcff91ca4e7b48210d286a
Rafał <Rafikus@users.noreply.github.com>
2019-05-22 10:57:25 +0200

    Add files via upload

---
+++ precache-manifest.9cb05f72a8d32c019ca7700105d22aba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1a1e204137fb45ace65a3618d0450a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af78783ad898e2e19954",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "50347e26e11aa0a4a69c",
+    "url": "/static/js/2.1fc44a9d.chunk.js"
+  },
+  {
+    "revision": "af78783ad898e2e19954",
+    "url": "/static/js/main.1bfbe28f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
